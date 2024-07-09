985bc377205f08f49c9e30d8fc8b1f11e4cec111
M-Erayman <muhammedderayman@gmail.com>
2024-07-09 11:16:44 +0300

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,53 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'passwordGenerate';
+  password: string = '';
+  length: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+
+  modifyLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  modifyLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  modifyNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  modifySymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  buttonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%#^&*()';
+    let validchars = '';
+    if (this.includeLetters) {
+      validchars += letters;
+    }
+    if (this.includeNumbers) {
+      validchars += numbers;
+    }
+    if (this.includeSymbols) {
+      validchars += symbols;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validchars.length); //Math.random() : 0 ile 1 arasinda sayi uretir...
+      generatedPassword += validchars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
